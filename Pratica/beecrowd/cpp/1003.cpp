ba06728001e360f12737e5ae474c1992201bc5e3
httpdluc <http.dluc@gmail.com>
2026-05-25 17:51:16 -0300

    beecrowd cpp 05/2026

---
+++ Pratica/beecrowd/cpp/1003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+    
+    int A, B, soma;
+    
+    cin >> A >> B;
+    
+    soma = A + B;
+
+    cout << "SOMA = " << soma << endl;
+    
+    return 0;
+}

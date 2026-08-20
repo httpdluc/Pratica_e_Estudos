4b481e44617d4caa2d06d79718107b063d7fedd1
httpdluc <http.dluc@gmail.com>
2026-08-20 20:55:30 -0300

    Lógica finalizada

--- Pratica/calculadora/calculadora.js
+++ Pratica/calculadora/calculadora.js
@@ -1,27 +1,109 @@
-const res = document.querySelector('.js-visor')
+const digito = document.querySelector('.js-visor')
 
 const numeros = document.querySelector('.js-numeros');
 const sinais = document.querySelector('.js-sinais');
 const deletar = document.querySelector('.js-deletar');
 
-let numero='';
-let sinal='';
+numeros.addEventListener('click', (e) => {
+  const valor = e.target.value;
+  valores(valor);
+})
 
+sinais.addEventListener('click', (e) => {
+  const valor = e.target.value;
+  valores(valor)
+})
 
-numeros.addEventListener('click', (event) => {
-  const sel = event.target.value;
-  if(sel!== undefined && !isNaN(sel) || sel === '.'){
-    numero += sel
-    res.innerHTML = `<p>${numero}</p>`
+deletar.addEventListener('click', (e) => {
+  const valor = e.target.value;
+  valores(valor)
+})
+
+const primeiraParte = [];
+const segundaParte = [];
+
+let sinal = '';
+
+let controle = true;
+let resultado ='';
+
+function valores(valor){
+  if(valor==='ac'){
+      primeiraParte.length = 0;
+      segundaParte.length = 0;
+      sinal='';
+      resultado='';
+      controle = true;
+      resposta()
   }
-})
+  if(controle === true){
+    if(!isNaN(valor)){
+      primeiraParte.push(valor)
+    }else if(valor === '.' && primeiraParte.length > 0 && !primeiraParte.includes('.')){
+      primeiraParte.push(valor)
+    }else if(valor==='del'){
+      primeiraParte.pop()
+    }else if ((valor === '+' ||
+               valor === '-' ||
+               valor === '*' ||
+               valor === '/' ||
+               valor === '%') &&
+               !isNaN(primeiraParte[primeiraParte.length - 1])
+              ){ 
+                sinal = valor; 
+                controle = false;
+    }
+  }else{
+    if(!isNaN(valor)){
+      segundaParte.push(valor)
+      calculo()
+    }else if(valor === '.' && segundaParte.length > 0 && !segundaParte.includes('.')){
+      segundaParte.push(valor)
+    }else if(valor==='del'){
+      segundaParte.pop()
+      if(segundaParte.length === 0){
+        sinal='';
+        resultado=''
+        controle = true;
+        resposta()
+      }
+    }else if(valor === '='){
+      calculo()
+    }
+  }
+  resposta()
+}
 
-sinais.addEventListener('click', (event) => {
-  const sin = event.target.value;
-  sinal+= sin;
-  res.innerHTML = `<p>${sinal}</p>`
-})
+function calculo(){
 
-function selecionado (){
-  console.log('selecionado')
+  const primeiroNumero = Number(primeiraParte.join(''));
+  const segundoNumero = Number(segundaParte.join(''));
+
+  if(sinal === '%'){
+    resultado = primeiroNumero*(segundoNumero/100);
+  }else if(sinal ==='/'){
+    resultado = primeiroNumero / segundoNumero;
+  }else if(sinal ==='*'){
+    resultado = primeiroNumero * segundoNumero;
+  }else if(sinal === '-'){
+    resultado = primeiroNumero - segundoNumero;
+  }else{
+    resultado = primeiroNumero + segundoNumero;
+  }
+}
+
+function resposta(){
+  let texto = '';
+
+  for(let i=0;i<primeiraParte.length; i++){
+    texto +=primeiraParte[i]
+  }
+
+  texto += sinal;
+
+  for(let i=0;i<segundaParte.length; i++){
+    texto +=segundaParte[i]
+  }
+
+  digito.innerHTML = `<p>${texto}</p>\n <p>${resultado}</p>`;
 }

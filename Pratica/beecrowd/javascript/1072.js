d60682541b384af17cebf3ed60ee040ed92c68b9
httpdluc <http.dluc@gmail.com>
2026-09-05 23:59:37 -0300

    Exercício 1072

---
+++ Pratica/beecrowd/javascript/1072.js
@@ -0,0 +1,23 @@
+class Main {
+  static execute() {
+    const controle = Number(lines[0]);
+
+    let dentro = 0;
+    let fora = 0;
+
+    for (let i = 1; i <= controle; i++) {
+      const numero = Number(lines[i]);
+
+      if (numero >= 10 && numero <= 20) {
+        dentro++;
+      } else {
+        fora++;
+      }
+    }
+
+    console.log(`${dentro} in\n${fora} out`);
+  }
+}
+
+Main.execute();
+
